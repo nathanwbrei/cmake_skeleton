fbcedee38bcd8cc67947f90be6d2ce86dfe90233
Nathan Brei <nbrei@jlab.org>
2024-08-02 15:16:14 -0400

    In-tree plugins can find each others' headers

---
+++ MyProject/Plugins/PluginA/plugina.cpp
@@ -0,0 +1,7 @@
+#include "plugina.hpp"
+#include <iostream>
+
+void say_hello_from_plugina() {
+    std::cout << "Hello from plugin a" << std::endl;
+}
+
---
+++ MyProject/Plugins/PluginB/pluginb.cpp
@@ -0,0 +1,10 @@
+
+#include "plugina.hpp"
+#include "pluginb.hpp"
+#include <iostream>
+
+void say_hello_from_pluginb() {
+    std::cout << "Hello from plugin b. Calling into plugin a..." << std::endl;
+    say_hello_from_plugina();
+}
+
